86d8e1b57c85667c5c4571908d942db8fbff3161
Bo Dong <zillionk1973@gmail.com>
2018-07-10 22:48:39 -0600

    add file exists check, add integer number check, move file header to parameter of the method. WAN

--- data_processor.py
+++ data_processor.py
@@ -11,9 +11,10 @@ class DataProcessor(object):
     """
     @staticmethod
     def load_file(input_file):
-        with open(input_file) as csv_file:
-            data = csv_file.readlines()
-            return data
+        if os.path.isfile(input_file):
+            with open(input_file) as csv_file:
+                data = csv_file.readlines()
+                return data
 
     """filter each person and get the highest salary record for each person.
         By default , the input records are sorted"""
@@ -33,25 +34,34 @@ class DataProcessor(object):
             else:
                 old_words = result_dict.get(words[self.INDEX_OF_EMPLOYEE_ID]).split(",")
                 
-                if int(words[self.INDEX_OF_SALARY]) > int(old_words[self.INDEX_OF_SALARY]):
+                if self.is_int(words[self.INDEX_OF_SALARY]) and self.is_int(old_words[self.INDEX_OF_SALARY]) and \
+                        int(words[self.INDEX_OF_SALARY]) > int(old_words[self.INDEX_OF_SALARY]):
                     result_dict[words[self.INDEX_OF_EMPLOYEE_ID]] = line
 
         return result_dict
         
     """Export the filtered result to a file"""
     @staticmethod
-    def export_result(result_dict, output_path):
-        with open(output_path, 'w') as csv_file:
-            fieldnames = ['RecordId', 'EmployID', 'Name', 'Age', 'Year', 'Salary', 'Type']
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-            writer.writeheader()
-            for key in result_dict:
-                csv_file.write(result_dict.get(key))
+    def export_result(result_dict, output_file, fieldnames=['RecordId', 'EmployID', 'Name', 'Age', 'Year', 'Salary', 'Type']):
+        if os.path.isfile(output_file):
+            with open(output_file, 'w') as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                writer.writeheader()
+                for key in result_dict:
+                    csv_file.write(result_dict.get(key))
+
+    @staticmethod
+    def is_int(value):
+        try:
+            int(value)
+            return True
+        except ValueError:
+            return False
 
 
 def main(argv):
     data_p = DataProcessor()
-    data = data_p.load_file
+    data = data_p.load_file("./testInput.csv")
     data_p.export_result(data_p.filter_info(data), "./filteredOutput.csv")
 
 
