3ca5764180d4bd3765f150c3e2c1435e1497be0b
Bo Dong <zillionk1973@gmail.com>
2018-07-10 20:53:02 -0600

    Refactor the file. WAN

--- data_processor.py
+++ data_processor.py
@@ -1,60 +1,55 @@
 import os,sys,csv
 
 class DataProcessor(object):
-    '''The constructor of the DataProcessor'''
-    def __init__(self, input_file):
-        self.input_file = input_file
 
-    '''load_file method loads the file from current folder and check if it's valid , with open will close file if any excetption happed '''
-    def load_file(self):
-        with open(self.input_file) as f:
-            data=f.readlines()
-            for line in data:
-                print(line)
+    '''load_file method loads the file from the current folder and check if it's valid , with open will close file if any
+    excetption happend '''
+    def load_file(self, input_file):
+        with open(input_file) as csv_file:
+            data=csv_file.readlines()
             return data
 
     '''filter each person and get the highest salary record for each person.
         By default , the input records are sorted.'''
-    def filterInfo(self, data):
-        resultDict={}
-        firstline = True
-        for line in data[:-1]:
-            if firstline:
-                firstline = False
-                continue
-            
+    def filter_info(self, data):
+        result_dict={}
+
+        LEGAL_LINE_LENGTH = 7
+        INDEX_OF_SALARY = 5
+        INDEX_OF_EMPLOYEE_ID = 1
+
+        '''Skip the header of the file.'''
+        for line in data[1:]:            
             words = line.split(",")
-            print(words[5])
+
+            '''If the current line is not long enough then skip'''
+            if len(words) != LEGAL_LINE_LENGTH:
+                continue 
             
-            if words[1] not in resultDict:
-            	resultDict[str(words[1])]=line
+            if words[INDEX_OF_EMPLOYEE_ID] not in result_dict:
+                result_dict[words[INDEX_OF_EMPLOYEE_ID]] = line
             else:
-                print("test value "+words[1])
-                print(resultDict.get(''+words[1]))
-                oldWords = resultDict.get(words[1]).split(",")
+                old_words = result_dict.get(words[INDEX_OF_EMPLOYEE_ID]).split(",")
                 
-                if int(words[5])> int(oldWords[5]):
-                    resultDict[''+str(words[1])]=line
-            print("Dict: ")
-            print(resultDict)
+                if int(words[INDEX_OF_SALARY])> int(old_words[INDEX_OF_SALARY]):
+                    result_dict[str(words[INDEX_OF_EMPLOYEE_ID])] = line
+        return result_dict
         
     '''Export the filted result to a file'''     
-    def export_result(self,resultDict):
-        with open('names.csv', 'w') as csvfile:
-            fieldnames = ['first_name', 'last_name']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
+    def export_result(self, result_dict, output_path):
+        with open(output_path, 'w') as csv_file:
+            fieldnames = ['RecordId', 'EmployID', 'Name', 'Age', 'Year', 'Salary', 'Type']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writeheader()
-            writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
-            writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
-            writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
-           	        
-        
+            for key in result_dict:
+                csv_file.write(result_dict.get(key))
+
+
 def main(argv):
-    dataP1 = DataProcessor("./testInput.csv")
-    data = dataP1.load_file()
-    dataP1.filterInfo(data)
-    dataP1.export_result(data)
+    dataP1 = DataProcessor()
+    data = dataP1.load_file("./testInput.csv")
+    dataP1.export_result(dataP1.filter_info(data), "./filteredOutput.csv")
+
 
 if __name__=="__main__":
     main(sys.argv[1:]) 
