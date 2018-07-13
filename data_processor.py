7a331942f4857e056a8056d08e9466121615fb71
Bo Dong <zillionk1973@gmail.com>
2018-07-12 22:16:11 -0600

    add more test cases for load file. WAN

--- data_processor.py
+++ data_processor.py
@@ -1,4 +1,6 @@
-import os,sys,csv
+import os
+import sys
+import csv
 
 
 class DataProcessor(object):
@@ -9,6 +11,7 @@ class DataProcessor(object):
     load_file method loads the file from the current folder and check if it's valid , with open will close file if any
     exception happened
     """
+
     @staticmethod
     def load_file(input_file):
         if os.path.isfile(input_file):
@@ -20,6 +23,7 @@ class DataProcessor(object):
 
     """filter each person and get the highest salary record for each person.
         By default , the input records are sorted"""
+
     def filter_info(self, data):
         result_dict = {}
 
@@ -27,33 +31,47 @@ class DataProcessor(object):
             raise TypeError("The type of the data is not list.")
 
         """skip the header of the file"""
-        for line in data[1:]:            
+        for line in data[1:]:
+            if not isinstance(line, str):
+                line = str(line)
+
             words = line.split(",")
 
-            """if the current line is not long enough then skip"""
+            """if the current line is not long enough or too long then skip the line"""
             if len(words) != self.LEGAL_LINE_LENGTH:
-                continue 
-            
+                continue
+
+            """TODO let Bo check"""
+            if not self.is_int(words[self.INDEX_OF_EMPLOYEE_ID]):
+                raise ValueError("The type of the employee id is not integer.")
+
             if words[self.INDEX_OF_EMPLOYEE_ID] not in result_dict:
                 result_dict[words[self.INDEX_OF_EMPLOYEE_ID]] = line
             else:
                 old_words = result_dict.get(words[self.INDEX_OF_EMPLOYEE_ID]).split(",")
-                
+
                 if self.is_int(words[self.INDEX_OF_SALARY]) and self.is_int(old_words[self.INDEX_OF_SALARY]) and \
                         int(words[self.INDEX_OF_SALARY]) > int(old_words[self.INDEX_OF_SALARY]):
                     result_dict[words[self.INDEX_OF_EMPLOYEE_ID]] = line
 
         return result_dict
-        
+
     """Export the filtered result to a file"""
+
     @staticmethod
-    def export_result(result_dict, output_file, fieldnames=['RecordId', 'EmployID', 'Name', 'Age', 'Year', 'Salary', 'Type']):
+    def export_result(result_dict, output_file,
+                      fieldnames=['RecordId', 'EmployID', 'Name', 'Age', 'Year', 'Salary', 'Type']):
         if os.path.isfile(output_file):
             with open(output_file, 'w') as csv_file:
                 writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
                 writer.writeheader()
+                if not isinstance(result_dict, dict):
+                    raise DictTypeException("The type of the result_dict is not dictionary.")
+
                 for key in result_dict:
                     csv_file.write(result_dict.get(key))
+        else:
+            raise TypeError("The output file is valid.")
 
     @staticmethod
     def is_int(value):
@@ -64,11 +82,14 @@ class DataProcessor(object):
             return False
 
 
+class DictTypeException(Exception):
+    pass
+
 def main(argv):
     data_p = DataProcessor()
     data = data_p.load_file("./testInput.csv")
     data_p.export_result(data_p.filter_info(data), "./filteredOutput.csv")
 
 
-if __name__=="__main__":
-    main(sys.argv[1:]) 
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- data_processor_test.py
+++ data_processor_test.py
@@ -1,11 +1,11 @@
 import unittest
-from mock import Mock
-from numpy.core.tests.test_scalarinherit import D
 
-from data_processor import DataProcessor
+from data_processor import DataProcessor, DictTypeException
+
 
 class TestDataProcessor(unittest.TestCase):
 
+    """Test load_file method"""
     def test_load_file_when_file_is_empty(self):
         data = DataProcessor.load_file("")
         self.assertEqual(None, data)
@@ -14,9 +14,81 @@ class TestDataProcessor(unittest.TestCase):
         data = DataProcessor.load_file("not_exist.csv")
         self.assertEqual(None, data)
 
+    """Test filter_info method"""
     def test_filter_info_wrong_data_format(self):
         data = '16983624@#!$@#$@#$'
         test_processor = DataProcessor()
         with self.assertRaises(TypeError):
             test_processor.filter_info(data)
 
+    def test_filter_info_empty_data(self):
+        data = []
+        test_processor = DataProcessor()
+        self.assertEqual({}, test_processor.filter_info(data))
+
+    def test_filter_info_empty_string_data(self):
+        data = ['']
+        test_processor = DataProcessor()
+        self.assertEqual({}, test_processor.filter_info(data))
+
+    def test_filter_info_right_data_not_long_enough(self):
+        data = [1, 1]
+        test_processor = DataProcessor()
+        self.assertEqual({}, test_processor.filter_info(data))
+
+    def test_filter_info_right_data_extra_column(self):
+        data = ['RecordId,EmployID,Name,Age,Year,Salary,Type', '10021,1,Rob,23,2008,65580,Sport,ExtraData']
+        test_processor = DataProcessor()
+        self.assertEqual({}, test_processor.filter_info(data))
+
+    def test_filter_info_right_data_no_header(self):
+        data = ['10021,1,Rob,23,2008,65580,Sport']
+        test_processor = DataProcessor()
+        self.assertEqual({}, test_processor.filter_info(data))
+
+    def test_filter_info_right_data_double_header(self):
+        data = ['RecordId,EmployID,Name,Age,Year,Salary,Type', 'RecordId,EmployID,Name,Age,Year,Salary,Type']
+        test_processor = DataProcessor()
+        with self.assertRaises(ValueError):
+            test_processor.filter_info(data)
+
+    def test_filter_info_right_data_double_empty_header(self):
+        data = ['', '']
+        test_processor = DataProcessor()
+        self.assertEqual({}, test_processor.filter_info(data))
+
+    def test_filter_info_right_data_all_int(self):
+        data = [0, 0, 0, 0, 0, 0, 0]
+        test_processor = DataProcessor()
+        self.assertEqual({}, test_processor.filter_info(data))
+
+    def test_filter_info_right_data_nest_list(self):
+        data = [[], 0, [], [], 0, 0, 0]
+        test_processor = DataProcessor()
+        self.assertEqual({}, test_processor.filter_info(data))
+
+    def test_filter_info_right_data_nest_list_but_right_employid_and_salary(self):
+        data = [[], 0, [], [], 0, 0, 0]
+        test_processor = DataProcessor()
+        self.assertEqual({}, test_processor.filter_info(data))
+
+    def test_filter_info_right_data(self):
+        data = ['RecordId,EmployID,Name,Age,Year,Salary,Type', '10021,1,Rob,23,2008,65580,Sport']
+        test_processor = DataProcessor()
+        self.assertEqual({'1': '10021,1,Rob,23,2008,65580,Sport'}, test_processor.filter_info(data))
+
+    """Test export_result method"""
+    def test_export_file_when_file_is_empty(self):
+        with self.assertRaises(TypeError):
+            DataProcessor.export_result({}, "")
+
+    def test_export_file_when_file_not_exist(self):
+        with self.assertRaises(TypeError):
+            DataProcessor.export_result({}, "not_exist.csv")
+
+    def test_export_file_when_not_dict(self):
+        with self.assertRaises(DictTypeException):
+            DataProcessor.export_result("wrong_data", "./test_filteredOutput.csv")
+
+    def test_export_file_when_dict_right_value(self):
+        DataProcessor.export_result({'1': '10021,1,Rob,23,2008,65580,Sport'}, "./test_filteredOutput.csv")
